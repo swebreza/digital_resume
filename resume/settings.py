2a396803f35db0f690914eeab807d68c16786b7a
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2021-11-20 19:19:12 +0530

    updates setting.py

--- resume/settings.py
+++ resume/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Application definition
 
