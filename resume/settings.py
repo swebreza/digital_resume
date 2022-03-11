11dd75a85912e787a604be1a5275a7ab5561ce0a
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2022-03-11 16:33:19 +0530

    deploy test 1

--- resume/settings.py
+++ resume/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1^ft*u+t&u-$!l!@ud^t*hdgz9f##m(7)0wbf&bf@+wn@3h1ga'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['digital-resume-suweb.herokuapp.com','127.0.0.1']
 
 
 # Application definition
