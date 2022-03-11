f1cf0c1405a0c332411ed4261460ba7267e1842e
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2022-03-11 15:08:17 +0530

    debugger turned off

--- resume/settings.py
+++ resume/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1^ft*u+t&u-$!l!@ud^t*hdgz9f##m(7)0wbf&bf@+wn@3h1ga'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
