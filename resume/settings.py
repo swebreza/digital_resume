338cbe718d3c6330bc4ef73a83d284edf30a6960
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2022-03-11 18:34:58 +0530

    deploy test 10

--- resume/settings.py
+++ resume/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATICFILES_DIRS = ((os.path.join(BASE_DIR, 'static')), )
-# STATICFILES_DIRS=[
-#     os.path.join(BASE_DIR, 'static'),
-#     os.path.join(BASE_DIR, 'mediafiles'),
-#     ]
+# STATICFILES_DIRS = ((os.path.join(BASE_DIR, 'static')),(os.path.join(BASE_DIR, 'media')))
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'mediafiles'),
+    ]
     
 
 
