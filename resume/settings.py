55dd2635652747f339972cc11173bc3038176819
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2022-03-13 04:07:58 +0530

    static file bugs

--- resume/settings.py
+++ resume/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,21 +122,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 # STATICFILES_DIRS = ((os.path.join(BASE_DIR, 'static')),(os.path.join(BASE_DIR, 'media')))
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'mediafiles'),
-    ]
+
     
 
 
-STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media')
+]
 
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_ROOT = os.path.join(BASE_DIR , "mediafiles")
 MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
