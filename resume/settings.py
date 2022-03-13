63542430401dab6ba7bdac3511730e234602dc2e
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2022-03-13 06:21:30 +0530

    media file bugs

--- resume/settings.py
+++ resume/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'resume.context_processors.project_context',
+                'django.template.context_processors.media',
             ],
         },
     },
