6b05c7e90eefe767340948c51a9778c718495648
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2022-03-13 06:55:16 +0530

    updated admin interface

--- resume/settings.py
+++ resume/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['digital-resume-suweb.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'djangocms_admin_style',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
